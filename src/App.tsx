9a03b3c256214ee6f1d045d6918e2f1a7fb2ec50
Matheus Neves <matheusneves1989@gmail.com>
2020-05-01 17:27:45 -0300

    feat: structuring toasts

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
 
+import ToastContainer from './components/ToastContainer';
+
 import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
@@ -10,6 +12,7 @@ const App: React.FC = () => (
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+    <ToastContainer />
     <GlobalStyle />
   </>
 );
