fd966cef478861ba7e366eab91c2214bbd97c7e6
Matheus Neves <matheusneves1989@gmail.com>
2020-06-18 16:11:32 -0300

    feat: add prop loading in Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,12 +1,22 @@
 import React, { ButtonHTMLAttributes } from 'react';
+import { RiMailSendLine } from 'react-icons/ri';
 
-import { Container } from './styles';
+import { Container, LoaderContainer } from './styles';
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  loading?: boolean;
+};
 
-const Button: React.FC<ButtonProps> = ({ children, ...rest }) => (
+const Button: React.FC<ButtonProps> = ({ children, loading, ...rest }) => (
   <Container type="button" {...rest}>
-    {children}
+    {loading ? (
+      <LoaderContainer>
+        <span>Enviando</span>
+        <RiMailSendLine size={24} />
+      </LoaderContainer>
+    ) : (
+      children
+    )}
   </Container>
 );
 
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,5 +1,17 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { shade } from 'polished';
+
+const transition = keyframes`
+  0% {
+    opacity: 1;
+  }
+  50% {
+    opacity: .3;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
 
 export const Container = styled.button`
   background: #ff9000;
@@ -13,7 +25,21 @@ export const Container = styled.button`
   margin-top: 16px;
   transition: background-color 0.2s;
 
+  svg {
+    animation: ${transition} 2s ease-in-out infinite;
+  }
+
   &:hover {
     background: ${shade(0.2, '#ff9000')};
+  }
+`;
+
+export const LoaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  span {
+    margin-right: 8px;
   }
 `;
