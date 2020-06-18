ca89cef2d651fc8a7d6fa4bc6ada0239424a0c4b
Matheus Neves <matheusneves1989@gmail.com>
2020-06-18 13:58:09 -0300

    feat(forgot-password): add new page component

---
+++ src/pages/ForgotPassword/index.tsx
@@ -0,0 +1,96 @@
+import React, { useRef, useCallback } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+import { FiLogIn, FiMail } from 'react-icons/fi';
+import { useToast } from '../../hooks/toast';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background, AnimationContainer } from './styles';
+import getValidationErrors from '../../utils/getValidationErrors';
+
+interface ForgotPasswordFormData {
+  email: string;
+  password: string;
+}
+
+const ForgotPassword: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const { addToast } = useToast();
+
+  const history = useHistory();
+
+  const handleSubmit = useCallback(
+    async (data: ForgotPasswordFormData) => {
+      try {
+        formRef.current?.setErrors({});
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('E-mail obrigatório')
+            .email('Digite um e-mail válido'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        const { email } = data;
+
+        // recuperação de senha
+
+        // history.push('/dashboard');
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+          formRef.current?.setErrors(errors);
+        }
+
+        addToast({
+          type: 'error',
+          title: 'Erro na recuperação de senha',
+          description:
+            'Ocorreu um erro ao tentar realizar a recuperação de senha, tente novamente',
+        });
+      }
+    },
+    [addToast],
+  );
+
+  return (
+    <Container>
+      <Content>
+        <AnimationContainer>
+          <img src={logoImg} alt="GoBarber" />
+
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Recuperar senha</h1>
+
+            <Input
+              name="email"
+              icon={FiMail}
+              type="email"
+              placeholder="E-mail"
+            />
+
+            <Button type="submit">Recuperar</Button>
+          </Form>
+
+          <Link to="/">
+            <FiLogIn />
+            Voltar ao login
+          </Link>
+        </AnimationContainer>
+      </Content>
+
+      <Background />
+    </Container>
+  );
+};
+
+export default ForgotPassword;
